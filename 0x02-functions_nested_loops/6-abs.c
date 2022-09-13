77e9e1ec1715189b4d14fd1b192ec07f5dcbf232
noeline6 <noeline840@gmail.com>
2022-09-13 10:23:32 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,32 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
- * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
- * and resets mins
+ * _abs - function that computes the absolute value of an integer
+ * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
-void jack_bauer(void)
+int _abs(int c)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
-
-	while (hours <= 23)
+	if (c > 0 || c == 0)
 	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
+	return (c);
 	}
-	hours++;
-	minutes = 0;
-}
+	else
+	return (c * -1);
 }
