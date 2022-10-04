7aef1ce2b821aa7a781faadd5886248d95d8dfb0
noeline6 <noeline840@gmail.com>
2022-10-04 15:02:02 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,45 +1,33 @@
-
-Sign up
-afinesami
-/
-holbertonschool-low_level_programming
-Public
-Code
-Issues
-Pull requests
-1
-Actions
-Projects
-Security
-Insights
-holbertonschool-low_level_programming/0x0B-malloc_free/0-create_array.c
-@afinesami
-afinesami C - malloc, free
- 1 contributor
-26 lines (25 sloc)  480 Bytes
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
+
 /**
- * *create_array - creates an array of chars, and initializes it with a char
- * @size: size of the array
- * @c: char to be initialized with
- * Return: pointer to array or null
- **/
+ * create_array - Allocates a set amount of memory using malloc
+ * @size: Memory size to allocate
+ * @c: Character to write to each byte
+ * Return: Array of chars
+ */
+
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-	unsigned int i;
+	int i;
+	char *ch = (char *) malloc(size);
 
 	if (size == 0)
-		return (NULL);
-	str = malloc(sizeof(char) * size);
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (i < size)
 	{
-		str[i] = c;
-		i++;
+		return (NULL);
 	}
-	return (str);
+	else if (ch == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < (int)size; i++)
+		{
+			ch[i] = c;
+		}
+	}
+	ch[i] = '\0';
+	return (ch);
 }
